175751924f4ede8d1b504d2df4d9cf78c21dad6a
Revanth SR <143567879+Revangapeth2003@users.noreply.github.com>
2025-03-27 21:39:24 +0530

    Update Women.js

--- src/pages/Women.js
+++ src/pages/Women.js
@@ -1,7 +1,22 @@
 import React from "react";
 import WomenData from "../data/WomenData";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../store/CartSlice.js";
+
 
 const Women = () => {
+  const cartProducts = useSelector((state) => state.cart.cartItems);
+  const dispatch = useDispatch();
+
+  const addCart = (item) => {
+    dispatch(addToCart(item));
+  };
+
+  const removeCart = (itemId) => {
+    dispatch(removeFromCart(itemId));
+  };
+
+
   return (
     <div>
       {/* Bootstrap Carousel */}
@@ -69,7 +84,22 @@ const Women = () => {
                     Discover the latest styles in women's fashion with our
                     exclusive collection.
                   </p>
-                  <button className="btn btn-primary mt-auto">Shop Now</button>
+                  {/* <button className="btn btn-primary mt-auto">Shop Now</button> */}
+                  {cartProducts.find((items) => items.id === product.id) ? (
+                    <button
+                      onClick={() => removeCart(product)}
+                      className="btn btn-danger mt-auto"
+                    >
+                      Remove
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => addCart(product)}
+                      className="btn btn-primary mt-auto"
+                    >
+                      Add
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
