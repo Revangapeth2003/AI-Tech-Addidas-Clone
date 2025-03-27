89e7b06a295febcf19c66f07fc99c58ec716c30c
Revanth SR <143567879+Revangapeth2003@users.noreply.github.com>
2025-03-27 21:36:29 +0530

    Update CartSlice.js

--- src/store/CartSlice.js
+++ src/store/CartSlice.js
@@ -36,18 +36,36 @@ const cartSlice = createSlice({
 
     removeFromCart: (state, action) => {
       const id = action.payload;
-      const itemToRemove = state.cartItems.find((item) => item.id === id);
-
+      // console.log(id.id,'data from payload')
+      // console.log(state.cartItems,"filtering data")
+      const itemToRemove = state.cartItems.find((item) => item.id === id.id);
+      // console.log(initialState.cartItems,"on remove of data from redux")
+      console.log(itemToRemove,"data to be removed")
       if (itemToRemove) {
         state.totalQuantity -= itemToRemove.quantity;
         state.totalPrice -= itemToRemove.totalPrice;
-        state.cartItems = state.cartItems.filter((item) => item.id !== id);
+        state.cartItems = state.cartItems.filter((item) => item.id !== id.id);
+        // console.log(state.cartItems,"on remove of data from redux")
       }
+      //  console.log(state.cartItems,"data in redux after delete")
+      // state.cartItems = state.cartItems.filter(Items => Items.id !== action.payload.id);
 
       // localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
     },
+
+    updateQuantity:(state,action)=>{
+      const {id,quantity} = action.payload;
+      const itemToUpdate = state.cartItems.find(Item => Item.id === id);
+
+      if(itemToUpdate){
+        itemToUpdate.quantity = quantity;
+        // localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
+      }
+    }
   },
 });
 
-export const { addToCart, removeFromCart } = cartSlice.actions;
-export const cartReducer = cartSlice.reducer;
+export const { addToCart, removeFromCart ,updateQuantity } = cartSlice.actions;//men.women,kids,cart 
+export const cartReducer = cartSlice.reducer;//store
+
+
